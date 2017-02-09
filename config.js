78507618c28b180d06181250f7a14624a1879fbc
alexitaylor <alexi@bluesunrise.com>
2017-02-08 18:53:06 -0800

    new config

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 /*Gavin*/
 module.exports = {
-	consumer_key:         'p8HnGKzufqDOFhRhmwB4u1yOQ',
-  	consumer_secret:      'jEQLZHBFxfm5SP8hsDx6ElamZIsOe9cElUeAYgoSOVBbqTVl6m',
-  	access_token:         '828720893496942592-F187WBCpct5zoJ1WOna5OAQ7S0BNA8A',
-  	access_token_secret:  '1ZfnPJudNuDVlkFukn7L3reLCswqTV8iDCfvsXqyvOO8o',
+	consumer_key:         'GMTzcVFCJJCLGWSicHXtS98E3',
+  	consumer_secret:      'u57VZsCEivSoBfCef0a5vjMZvn00vfjz6wHSxqKiHT983QXwxG',
+  	access_token:         '828720893496942592-CVHBfMjPodQRZUCQ3yMMonWUGEh4WPg',
+  	access_token_secret:  '34BPZVBQRjM4VwbOr2JJwEpaFBZG0eaOWtSszFs6IZMNF',
   	timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
